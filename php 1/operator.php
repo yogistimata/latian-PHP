fb021d0428fe30be4092ddc1f8c2d3c25e268204
yogistimata <sasmitayogi8@gmail.com>
2021-11-25 15:54:33 +0700

    perintah tugas 5

---
+++ php 1/operator.php
@@ -0,0 +1,15 @@
+<?php
+$x = 20;
+$y = 10;
+
+$tambah = $x + $y;
+echo "hasil penjumlahan tamabah = $tambah";
+echo "<br>";
+$kali = $x * $y;
+echo "hasil penjumlahan kali = $kali";
+echo "<br>";
+$kurang = $x - $y;
+echo "hasil penjumlahan kurang = $kurang";
+
+
+?>
