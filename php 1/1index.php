16200b3d5cb6dba1130af66da078a76289ab040e
yogistimata <sasmitayogi8@gmail.com>
2021-11-25 15:09:45 +0700

    perintah tugas 2

---
+++ php 1/1index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="n">
+    <head>
+
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-compatible" content="IE=edge">
+        <meta name="viwport" content="width=device-width, initial-scale=1.0">
+        <title>belajar php</title>
+    </head>
+    <body>
+    <h1>SELAMAT DATANG
+    <?php echo"SAYA YOGI";?>
+    </h1>
+      
+
+
+    </body>
+
+</html>
