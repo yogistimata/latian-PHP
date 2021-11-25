65295e20df79b2d96cb852c0d4de940b8a26c0bf
yogistimata <sasmitayogi8@gmail.com>
2021-11-25 15:23:42 +0700

    perintah tugas 3

---
+++ php 1/3index.php
@@ -0,0 +1,22 @@
+<?php
+$nama = "YOGI";
+?>
+<!DOCTYPE html>
+<html lang="n">
+    <head>
+
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-compatible" content="IE=edge">
+        <meta name="viwport" content="width=device-width, initial-scale=1.0">
+        <title>belajar php</title>
+    </head>
+    <body>
+   
+    <h1>HALLO, SELAMAT DATANG, SAYA <?php echo $nama ; ?></h1>
+
+      
+
+
+    </body>
+
+</html>
