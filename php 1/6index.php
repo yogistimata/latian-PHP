5d095678fe2568ebf75b1c16f241a36a117a7747
yogistimata <sasmitayogi8@gmail.com>
2021-11-26 21:08:12 +0700

    php 2

---
+++ php 1/6index.php
@@ -0,0 +1,12 @@
+<?php
+var_dump(5 < 10 );
+echo <br>;
+echo "operator identitas";
+echo <br>;
+var_dump(5=="5");
+echo <br>;
+echo "operator logika";
+echo <br>;
+$x = 20;
+var_dump($x <10 && $x %2 ==0);
+?>
